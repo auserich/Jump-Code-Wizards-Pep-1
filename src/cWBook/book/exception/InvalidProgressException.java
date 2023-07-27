70beb9cf18c0244dc670daeede4aa5b73c34e302
Austin Rich <auserich@gmail.com>
2023-07-27 10:55:41 -0700

    Added some images/diagrams for presentation and custom exception for setting progress

---
+++ src/cWBook/book/exception/InvalidProgressException.java
@@ -0,0 +1,10 @@
+package cWBook.book.exception;
+
+public class InvalidProgressException extends Exception {
+
+	private static final long serialVersionUID = -7694196740027114657L;
+
+	public InvalidProgressException(String message) {
+		super(message);
+	}
+}
